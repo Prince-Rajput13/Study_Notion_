316bd653f3636a8a7542ef31327ac7a315b56fad
Prince-Rajput13 <prince.rajput4050@gmail.com>
2026-03-05 12:40:23 +0530

    Course/Section Model created, Tag Controller Created

---
+++ controllers/Courses.js
@@ -0,0 +1,8 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Tag");
+const User = require("../models/User");
+const imageUploader = require("../utils/ImageUploader");
+
+exports.createCourse = async (req,res) =>{
+    
+}
