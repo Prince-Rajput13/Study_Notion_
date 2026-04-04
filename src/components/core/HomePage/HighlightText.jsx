ebd72448501ad6b779e7fb6125b941790d14923c
Prince-Rajput13 <prince.rajput4050@gmail.com>
2026-04-05 01:05:04 +0530

    Buttons and banner added

---
+++ src/components/core/HomePage/HighlightText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HighlightText({text}) {
+  return (
+    <span>
+        {text}
+    </span>
+  )
+}
+
+export default HighlightText;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from "react-icons/fa";
+import HighlightText from "../components/core/HomePage/HighlightText";
+import CTAbutton from "../components/core/HomePage/CTAbutton";
+import Banner from "../assets/Images/banner.mp4";
 function Home() {
   return (
     <div>
         {/* Section1 */}
-        <div className='relative text-white w-11/12 mx-auto justify-between items-center flex flex-col'>
+        <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center 
+      text-white justify-between'>
             <Link to={"/signup"}>
-                <div>
-                    <div>
+                <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+            transition-all duration-200 hover:scale-95 w-fit'>
+                    <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
+                transition-all duration-200 group-hover:bg-richblack-900'>
                         <p>
                             Become an instructor
                             <FaArrowRight/>
@@ -16,6 +22,31 @@ function Home() {
                     </div>
                 </div>
             </Link>
+            <div className='text-center text-4xl font-semibold mt-7'>
+                Empower Your Future With 
+                <HighlightText text={" Coding Skills"}/>
+            </div>
+            <div className=' mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+                With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors. 
+            </div>
+            <div className='flex flex-row gap-7 mt-8'>
+                <CTAbutton active={true} linkto={"/signup"}>
+                Learn more
+                </CTAbutton>
+                <CTAbutton active={false} linkto={"/login"}>
+                Book a Demo
+                </CTAbutton>
+            </div>
+            <div className='mx-3 my-12 shadow-blue-200'>
+                <video
+                muted
+                loop
+                autoPlay
+                >
+                    <source  src={Banner} type="video/mp4" />
+                </video>
+            </div>
+
         </div>
         {/* Section2 */}
         {/* Section3 */}
