306ba5e70f54b14a66e3e9d3e594266f99fd8b01
Prince-Rajput13 <prince.rajput4050@gmail.com>
2026-06-16 16:14:20 +0530

    Navbar Fixed and tested

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -7,14 +7,33 @@ import { useSelector } from 'react-redux'
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import ProfileDropdown from '../core/Auth/ProfileDropDown';
 import state from 'state/lib/state';
+import { useEffect,useState } from 'react'
+import { FiChevronDown } from "react-icons/fi";
+import { apiConnector } from '../../services/apiconnector';
+import { categories } from '../../services/apis'
+
 function Navbar() {
   const {token} = useSelector( (state) => state.auth );
   const {user} = useSelector( (state) => state.profile );
   const {totalItems} = useSelector( (state) => state.cart);
+  const [subLinks, setSubLinks] = useState([])
   const location = useLocation()
+  const [loading, setLoading] = useState(false)
   const matchRoute=(route)=>{
       return matchPath({path:route},location.pathname);
   }
+  useEffect(() => {
+      ;(async () => {
+        setLoading(true)
+        try {
+          const res = await apiConnector("GET", categories.CATEGORIES_API)
+          setSubLinks(res.data.data)
+        } catch (error) {
+          console.log("Could not fetch Categories.", error)
+        }
+        setLoading(false)
+      })()
+    }, [])
   return (
     <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
       <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
@@ -27,7 +46,45 @@ function Navbar() {
               NavbarLinks.map( (link,index) =>(
                 <li key={index}>
                   {
-                    link.title=== "Catalog" ? (<div></div>):(
+                    link.title=== "Catalog" ? (
+                      <div
+                        className={`group relative flex cursor-pointer items-center gap-1 ${
+                          matchRoute("/catalog/:catalogName")
+                            ? "text-yellow-25"
+                            : "text-richblack-25"
+                        }`}
+                      >
+                        <p>{link.title}</p>
+                        <FiChevronDown />
+                        <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                          <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                          {loading ? (
+                            <p className="text-center">Loading...</p>
+                          ) : subLinks.length ? (
+                            <>
+                              {subLinks
+                                ?.filter(
+                                  (subLink) => subLink?.courses?.length > 0
+                                )
+                                ?.map((subLink, i) => (
+                                  <Link
+                                    to={`/catalog/${subLink.name
+                                      .split(" ")
+                                      .join("-")
+                                      .toLowerCase()}`}
+                                    className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                    key={i}
+                                  >
+                                    <p>{subLink.name}</p>
+                                  </Link>
+                                ))}
+                            </>
+                          ) : (
+                            <p className="text-center">No Courses Found</p>
+                          )}
+                        </div>
+                      </div>
+                    ):(
                       <Link to={ link?.path}>
                         <p className= {`${ matchRoute(link?.path) ? "text-yellow-25" :"text-richblack-25"}`}>
                           {link.title}
